7f264101a8ce2eee6141dcaee2d92918d137b78c
jin605 <jinddd3@gmail.com>
2026-04-08 15:29:03 +0900

    feat : 관찰 속성 실습

--- 01_first/src/components/Options.vue
+++ 01_first/src/components/Options.vue
@@ -16,6 +16,7 @@
         </p>
 
         <h4>2) 메소드</h4>
+        
         <p>
             <!-- 동일한 메소드를 여러 번 호출해서 사용한다. -->
             <!-- 동일한 값을 같은 화면에 여러 번 출력해야 할 경우에는 계산된 속성을 사용하는 것이 좋다. -->
@@ -23,6 +24,22 @@
             <br>
             1부터 입력한 값까지의 합: <span v-text="sum()"></span>
         </p>
+
+        <h4>3) 관찰 속성</h4>
+        <label for="x">x: </label>
+        <!-- <input type="number" id="x" v-model="x"> -->
+        <input type="number" id="x" v-model="values.x">
+
+        <br>
+
+        <label for="y">y: </label>
+        <!-- <input type="number" id="y" v-model="y"> -->
+        <input type="number" id="y" v-model="values.y">
+
+        <p>
+            두 수의 뺄샘 결과: <span v-text="sub"></span>
+        </p>
+
     </div>
 </template>
 
@@ -31,7 +48,14 @@
         name: 'Options',
         data() {
             return {
-                number: 0
+                number: 0,
+                // x: 0,
+                // y: 0,
+                values: {
+                    x: 0,
+                    y: 0
+                },
+                sub: 0
             }
         },
         computed: {
@@ -45,7 +69,35 @@
                 
             }
         },
-    }
+        watch: {
+            // x(newValue, oldValue) {
+            //     this.sub = newValue - this.y;
+            // },
+            // y(newValue, oldValue) {
+            //     this.sub = this.x - newValue;
+            // }
+            values: {
+                // 관찰 속성에 등록하던 함수를 handler 함수에 등록한다.
+                // handler: function(newValue, oldValue)
+                handler(newValue, oldValue) {
+                    this.sub = newValue.x - newValue.y;
+                    },
+                // 객체 내부의 값이 변경될 때 handler 함수가 실행되도록 지정하는 속성
+                deep: true
+                }
+            },
+
+            created () {
+                console.log("created() - 호출");
+            },
+            mounted () {
+                console.log("mounted() - 호출");
+            },
+            updated () {
+                console.log("updated() - 호출");
+            },
+        }
+
 </script>
 
 <style scoped>
