b0c346b1a5e27447ddfdab8e474991075a989e63
jin605 <jinddd3@gmail.com>
2026-04-10 17:14:12 +0900

    feat: login emit 추가

--- 04_university/src/components/forms/LoginForm.vue
+++ 04_university/src/components/forms/LoginForm.vue
@@ -1,16 +1,16 @@
 <template>
     <div>
-        <form>
+        <form @submit.prevent="submitClick">
             <h1 class="h3 mb-3 fw-normal">로그인 페이지</h1>
 
             <div class="form-floating">
                 <input type="text" class="form-control" id="floatingInput" placeholder="Username"
-                    required>
+                    v-model="formData.username" required>
                 <label for="floatingInput">Username</label>
             </div>
             <div class="form-floating">
                 <input type="password" class="form-control" id="floatingPassword" placeholder="Password"
-                    required>
+                    v-model="formData.password"required>
                 <label for="floatingPassword">Password</label>
             </div>
             <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
@@ -20,7 +20,18 @@
 </template>
 
 <script setup>
+import { reactive, toRaw } from 'vue';
 
+    const formData = reactive({
+        username: '',
+        password: ''
+    });
+
+    const emit = defineEmits(['form-submit']);
+
+    const submitClick = () => {
+        emit('form-submit', toRaw(formData));
+    };
 </script>
 
 <style scoped>
--- 04_university/src/views/auth/login.vue
+++ 04_university/src/views/auth/login.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="form-signin w-100 m-auto">
-        <LoginForm></LoginForm>
+        <LoginForm @form-submit="formSubmit"></LoginForm>
     </main>
 </template>
 
@@ -8,6 +8,11 @@
     import '@/assets/css/auth/sign-in.css'
     import LoginForm from '@/components/forms/LoginForm.vue';
 
+    const formSubmit = (formData) => {
+        alert('form-submit 이벤트 수신');
+
+        console.log(formData);
+    };
 </script>
 
 <style scoped>
