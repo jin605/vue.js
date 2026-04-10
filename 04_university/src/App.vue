fbcc4772af68073c0565f5ad0fc42ca7dd212d23
jin605 <jinddd3@gmail.com>
2026-04-10 10:48:51 +0900

    feat : 라우터 설정

--- 04_university/src/App.vue
+++ 04_university/src/App.vue
@@ -1,22 +1,29 @@
 <template>
   <div>
-    <!-- 요청된 URI 경로와 router 객체의 routes배열에 있는 경로를 비교하여 매치된 라우트의 컴포넌트를 마운트한다. -->
+    <!-- 화면 전환에 필요한 링크를 생성한다. -->
+    <!-- 명명된 라우터의 경우 v-bind를 이용해서 to 속성에 문자열이 아닌 객체를 바인딩해야 한다. -->
+    <RouterLink :to="{name: 'home'}">Home</RouterLink>
+    <br>
+    <RouterLink :to="{name: 'departments'}">학과 목록 조회</RouterLink>
+    <br>
+    <RouterLink :to="{name: 'departments/no', params: {no: '033'}}">학과 상세 조회</RouterLink>
+    <br>
+    <RouterLink :to="{name: 'departments/add'}">학과 등록</RouterLink>
+    <!-- 
+    요청된 URI 경로와 router 객체의 routes 배열에 있는 경로를 비교하여 매칭된 라우트의 컴포넌트를 마운트한다. 
+    -->
     <RouterView></RouterView>
+
   </div>
 </template>
 
-<script>
-import { RouterView } from 'vue-router';
+<script setup>
+import { RouterLink } from 'vue-router';
 
-export default {
-  setup () {
-    
 
-    return {}
-  }
-}
 </script>
 
 <style scoped>
 
 </style>
+
