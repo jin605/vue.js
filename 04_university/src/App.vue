3b06f17c25de757d785b9c39212457b4475985c7
jin605 <jinddd3@gmail.com>
2026-04-10 12:13:33 +0900

    feat : footer header 추가

--- 04_university/src/App.vue
+++ 04_university/src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <Header></Header>
+
     <!-- 화면 전환에 필요한 링크를 생성한다. -->
     <!-- 명명된 라우터의 경우 v-bind를 이용해서 to 속성에 문자열이 아닌 객체를 바인딩해야 한다. -->
     <RouterLink :to="{name: 'home'}">Home</RouterLink>
@@ -13,12 +15,15 @@
     요청된 URI 경로와 router 객체의 routes 배열에 있는 경로를 비교하여 매칭된 라우트의 컴포넌트를 마운트한다. 
     -->
     <RouterView></RouterView>
-
+    
+    <Footer></Footer>
   </div>
 </template>
 
 <script setup>
 import { RouterLink } from 'vue-router';
+import Header from './components/common/Header.vue';
+import Footer from './components/common/Footer.vue';
 
 
 </script>
