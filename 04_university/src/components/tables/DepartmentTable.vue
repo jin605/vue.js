36bce699697bd24bd5e7e677f7221ab0ad96252a
jin605 <jinddd3@gmail.com>
2026-04-13 12:43:01 +0900

    feat : 페이지 네이션 추가

---
+++ 04_university/src/components/tables/DepartmentTable.vue
@@ -0,0 +1,34 @@
+<template>
+    <table class="table table-striped table-hover text-center">
+        <thead>
+            <tr>
+                <th>학과 번호</th>
+                <th>학과 이름</th>
+                <th>계열</th>
+                <th>개설 여부</th>
+                <th>정원</th>
+                <th>삭제</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>001</td>
+                <td>aaa</td>
+                <td>bbb</td>
+                <td>ccc</td>
+                <td>ddd</td>
+                <td>
+                    <button type="button" class="btn btn-outline-danger">삭제</button>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- 04_university/src/views/department/Departments.vue
+++ 04_university/src/views/department/Departments.vue
@@ -1,46 +1,35 @@
 <template>
     <main>
-        <h2>학과 목록 조회</h2>
-
+        <DepartmentTable/>
+        <Pagination/>
     </main>
 </template>
 
 <script setup>
-    import apiClient from '@/api';
-import { onMounted } from 'vue';
-    // axios 사용법
-    // const fetchDepartments = () => {
-    //     apiClient.get('/api/v1/department-service/departments?page=1&numOfRows=10')
-    //     // 비동기 통신이 성공적으로 완료되었을 때 호출되는 콜백 함수를 지정한다.
-    //     .then((response) => {
-    //         console.log(response);
-    //     })
-    //     // 비동기 통신이 실패했을 때 호출되는 콜백 함수를 지정한다.
-    //     .catch((error) => {
-    //         console.log(error);
-    //     });
+    import { onMounted } from 'vue';
+    import DepartmentTable from '@/components/tables/DepartmentTable.vue';
+    import Pagination from '@/components/common/Pagination.vue';
+    import { useDepartmentStore } from '@/stores/departmentStore';
 
-    // async / await 사용
-    //  - 자바 스크립트에서 비동기 작업을 효과적으로 처리할 수 있다.
-    //  - 직접 axios를 사용할 떄와 비교해 콜백 함수, 예외 처리가 간결해진다.
-    //  - async는 비동기 작업을 포함하는 함수의 앞부분에 작성한다.
-    //  - await는 async 함수 내에서만 작성할 수 있고 비동기 작업의 완료를 기다린다.
-    const fetchDepartments = async () => {
+    const departmentStore = useDepartmentStore();
+
+    onMounted(async() => {
         try {
-            const response = 
-                await apiClient.get('/api/v1/department-service/departments?page=1&numOfRows=10');
 
-            console.log(response);
+            const response = await departmentStore.fetchDepartments(1,10);
             
         } catch (error) {
+            const {status, message} = error.response.data;
             
-            console.log(error)
+            if (status === 'DEPARTMENT_NOT_FOUND') {
+                alert(message);
+            } else if (status === 'INTERNAL_SERVER_ERROR') {
+                alert('에러가 발생하였습니다.');
+            }   
+            
         }
-    };
+    });
 
-    onMounted(() => {
-        fetchDepartments();
-    })
 </script>
 
 <style scoped>
